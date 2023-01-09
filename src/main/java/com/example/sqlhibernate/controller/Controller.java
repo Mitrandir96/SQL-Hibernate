3d2c906a42d322a50856b6f1da33f1eaeccd2a1b
Илья Грачев <grachev.ia96@gmail.com>
2023-01-10 01:09:47 +0700

    added enablesecurity

--- src/main/java/com/example/sqlhibernate/controller/Controller.java
+++ src/main/java/com/example/sqlhibernate/controller/Controller.java
@@ -2,9 +2,15 @@ package com.example.sqlhibernate.controller;
 
 import com.example.sqlhibernate.entities.Person;
 import com.example.sqlhibernate.repository.Repository;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,4 +37,31 @@ public class Controller {
     public Optional<Person> findPersonByNameAndSurname(String name, String surname) {
         return repository.findPersonByNameAndSurname(name, surname);
     }
+
+    @Secured({"ROLE_READ"})
+    @GetMapping("/read")
+    public String read() {
+        return "Read";
+    }
+
+    @RolesAllowed({"ROLE_WRITE"})
+    @GetMapping("/write")
+    public String write() {
+        return "Write";
+    }
+    @PreAuthorize("hasAnyRole('DELETE')")
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+
+
+    @GetMapping("/authUser")
+    @PreAuthorize("#username == authentication.principal.username")
+    public String greetingUser(String username) {
+        System.out.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        return "Hello from secure app, " + username;
+    }
+
+
 }
--- src/main/java/com/example/sqlhibernate/security/SecurityConfig.java
+++ src/main/java/com/example/sqlhibernate/security/SecurityConfig.java
@@ -1,30 +1,36 @@
 package com.example.sqlhibernate.security;
 
-import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("Ilya").password("{noop}password").authorities("age", "fullname");
+                .withUser("Ilya")
+                .password(encoder().encode("password"))
+                .roles("READ", "WRITE")
+                .and()
+                .withUser("Sergey")
+                .password(encoder().encode("password2"))
+                .roles("READ", "DELETE")
+                .and()
+                .withUser("Ivan")
+                .password(encoder().encode("password3"))
+                .roles("WRITE", "DELETE");
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-city").permitAll()
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-age").hasAuthority("age")
-                .and()
-                .authorizeRequests().antMatchers("/persons/by-fullname").hasAuthority("fullname")
-                .and()
-                .authorizeRequests().anyRequest().authenticated();
-    }
 }
